4155a37a2846868f9d1257a33db63275932755b4
GI <az0.mint9@gmail.com>
2017-12-30 12:33:32 +0900

    First Commit

---
+++ src/main/scala/jp.beginner.roppongi/model/domain/support/EmployeeID.scala
@@ -0,0 +1,5 @@
+package jp.beginner.roppongi.model.domain.support
+
+case class EmployeeID(value: String) extends AnyVal {
+
+}
---
+++ src/main/scala/jp.beginner.roppongi/model/domain/support/Identifier.scala
@@ -0,0 +1,5 @@
+package jp.beginner.roppongi.model.domain.support
+
+trait Identifier {
+
+}
